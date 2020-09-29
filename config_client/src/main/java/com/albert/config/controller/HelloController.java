e686e955e044071d3b871b1bec18832576a3077d
Albert <2495774179@qq.com>
2020-09-29 16:28:47 +0800

    we

--- config_client/src/main/java/com/albert/config/controller/HelloController.java
+++ config_client/src/main/java/com/albert/config/controller/HelloController.java
@@ -1,4 +1,17 @@
 package com.albert.config.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @Value("${springcloud.hello}")
+    private String hello;
+
+    @RequestMapping("hello")
+    public String from(){
+        return this.hello;
+    }
 }
